1a5737a58dd0eb4b0717070a7b2d2540f1b85d08
angeljonathan5675-design <angeljonathan5675@gmail.com>
2026-02-04 12:03:47 -0800

    proceso de encriptacion

--- interfaz.py
+++ interfaz.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
 from Avility.molina import molina_facturacion
 from Avility.silversumit import silversumit_facturacion
 from Medicaid.medicaid import medicaid_facturacion
